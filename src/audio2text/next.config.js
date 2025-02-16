09194cfcc791332b2a32942b8b62590cac242653
LoumLabs <your.email@example.com>
2025-02-17 00:27:53 +0100

    chore: update build configuration and gitignore
    - Update Next.js configuration
    - Add build and cache files to gitignore
    - Update Netlify configuration

--- src/audio2text/next.config.js
+++ src/audio2text/next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: '/audio2text',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/audio2text' : '',
+  distDir: '../../audio2text',
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/audio2text/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
