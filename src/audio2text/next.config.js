078d3b8c456c8fe2f85e291bcc5f6c62be87432e
LoumLabs <your.email@example.com>
2025-02-17 00:33:56 +0100

    fix: restore basePath for production URL

--- src/audio2text/next.config.js
+++ src/audio2text/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: '/audio2text',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/audio2text' : '',
   images: {
     unoptimized: true
   },
